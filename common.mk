d81d61f5f7fa3dd1abb33e1ad9e725ececdd3d22
EmanuelCN <emanuelghub@gmail.com>
2024-06-03 20:19:00 +0000

    sm8250-common: Override task_profiles.json for uclamp kernel task placement
    In order to allow uclamp kernels to place tasks correctly.
    Taken from AOSP and moved SurfaceFlinger to foreground cpuset.
    
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- common.mk
+++ common.mk
@@ -306,6 +306,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus
 
+# Task profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
